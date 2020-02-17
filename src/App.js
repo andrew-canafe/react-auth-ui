110318cffe847a500df479061b5fa012e82853b3
Andrew <andrew.g.canafe@gmail.com>
2020-02-16 20:05:09 -0800

    Add sign up and login functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { HashRouter as Router, Route, Link, NavLink } from 'react-router-dom';
+import { HashRouter as Router, Route, NavLink } from 'react-router-dom';
 import SignUpForm from './pages/SignUpForm';
-import SignInForm from './pages/SignInForm';
+import LogInForm from './pages/LogInForm';
 
 import './App.css';
 
@@ -13,18 +13,14 @@ class App extends Component {
           <div className="App__Aside"></div>
           <div className="App__Form">
             <div className="PageSwitcher">
-                <NavLink to="/sign-in" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign In</NavLink>
-                <NavLink exact to="/" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
-              </div>
+              <NavLink to="/login" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Log In</NavLink>
+              <NavLink exact to="/" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
+            </div>
 
-              <div className="FormTitle">
-                  <NavLink to="/sign-in" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign In</NavLink> or <NavLink exact to="/" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign Up</NavLink>
-              </div>
-
-              <Route exact path="/" component={SignUpForm}>
-              </Route>
-              <Route path="/sign-in" component={SignInForm}>
-              </Route>
+            <Route exact path="/" component={SignUpForm}>
+            </Route>
+            <Route path="/login" component={LogInForm}>
+            </Route>
           </div>
 
         </div>
--- src/pages/SignUpForm.js
+++ src/pages/SignUpForm.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import axios from 'axios';
+//import { Link } from 'react-router-dom';
 
+//axios.defaults.baseURL = 'https://us-central1-maintenance-genie.cloudfunctions.net/api';
 class SignUpForm extends Component {
     constructor() {
         super();
 
         this.state = {
-            email: '',
-            password: '',
-            name: '',
-            hasAgreed: false
+          email: '',
+          password: '',
+          confirm_password: '',
+          full_name: '',
+          address: '',
         };
 
         this.handleChange = this.handleChange.bind(this);
@@ -18,7 +21,7 @@ class SignUpForm extends Component {
 
     handleChange(e) {
         let target = e.target;
-        let value = target.type === 'checkbox' ? target.checked : target.value;
+        let value = target.value;
         let name = target.name;
 
         this.setState({
@@ -29,8 +32,28 @@ class SignUpForm extends Component {
     handleSubmit(e) {
         e.preventDefault();
 
-        console.log('The form was submitted with the following data:');
-        console.log(this.state);
+        var body = new FormData();
+        body.append('email', this.state.email);
+        body.append('password', this.state.password);
+        body.append('confirm_password', this.state.confirm_password);
+        body.append('full_name', this.state.full_name);
+        //body.append('address', this.state.address);
+
+        axios({
+          method: 'post',
+          url: 'https://us-central1-maintenance-genie.cloudfunctions.net/api/signup',
+          data: body,
+          headers: {'Content-Type': 'application/json ', 'Access-Control-Allow-Origin': '*'}
+        })
+        .then((res) => {console.log(res)})
+        .catch((res) => {console.log(res)})
+
+        /*axios.post('/signup', this.state)
+        .then((res) => {console.log(res)})
+        .catch((res) => {console.log(res)});*/
+
+        //console.log('The form was submitted with the following data:');
+        //console.log(this.state);
     }
 
     render() {
@@ -38,26 +61,28 @@ class SignUpForm extends Component {
         <div className="FormCenter">
             <form onSubmit={this.handleSubmit} className="FormFields">
               <div className="FormField">
-                <label className="FormField__Label" htmlFor="name">Full Name</label>
-                <input type="text" id="name" className="FormField__Input" placeholder="Enter your full name" name="name" value={this.state.name} onChange={this.handleChange} />
+                <label className="FormField__Label" htmlFor="full_name">Full Name</label>
+                <input type="text" id="full_name" className="FormField__Input" placeholder="Enter your full name" name="full_name" value={this.state.name} onChange={this.handleChange} />
+              </div>
+              <div className="FormField">
+                <label className="FormField__Label" htmlFor="address">Address</label>
+                <input type="text" id="address" className="FormField__Input" placeholder="Enter your street address" name="address" value={this.state.name} onChange={this.handleChange} />
+              </div>
+              <div className="FormField">
+                <label className="FormField__Label" htmlFor="email">Email Address</label>
+                <input type="email" id="email" className="FormField__Input" placeholder="Enter your email address" name="email" value={this.state.email} onChange={this.handleChange} />
               </div>
               <div className="FormField">
                 <label className="FormField__Label" htmlFor="password">Password</label>
                 <input type="password" id="password" className="FormField__Input" placeholder="Enter your password" name="password" value={this.state.password} onChange={this.handleChange} />
               </div>
               <div className="FormField">
-                <label className="FormField__Label" htmlFor="email">E-Mail Address</label>
-                <input type="email" id="email" className="FormField__Input" placeholder="Enter your email" name="email" value={this.state.email} onChange={this.handleChange} />
+                <label className="FormField__Label" htmlFor="confirm_password">Confirm Password</label>
+                <input type="password" id="confirm_password" className="FormField__Input" placeholder="Enter your password again" name="confirm_password" value={this.state.confirm_password} onChange={this.handleChange} />
               </div>
 
-              <div className="FormField">
-                <label className="FormField__CheckboxLabel">
-                    <input className="FormField__Checkbox" type="checkbox" name="hasAgreed" value={this.state.hasAgreed} onChange={this.handleChange} /> I agree all statements in <a href="" className="FormField__TermsLink">terms of service</a>
-                </label>
-              </div>
-
-              <div className="FormField">
-                  <button className="FormField__Button mr-20">Sign Up</button> <Link to="/sign-in" className="FormField__Link">I'm already member</Link>
+              <div className="FormFieldCenter">
+                  <button className="FormField__Button mr-20">Sign Up</button>
               </div>
             </form>
           </div>
