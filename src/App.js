ab9c50108cb58c9e97c4b2a7a61235911407d82a
Andrew <andrew.g.canafe@gmail.com>
2020-02-20 03:27:47 -0800

    Fix CSS alignment issues

--- src/App.js
+++ src/App.js
@@ -15,20 +15,20 @@ class App extends Component {
         <div>
           <div className="App">
             <div className="App__Form">
-              <Route exact path="/" component={SignUpForm}>
-              </Route>
-              <Route path="/login" component={LogInForm}>
-              </Route>
-              <Route path="/createticket" component={CreateTicketForm}>
-              </Route>
-              <Route path="/tickethistory" component={TicketHistoryForm}>
-              </Route>
-              <Route path="/myaccount" component={MyAccountForm}>
-              </Route>
+                <Route exact path="/" component={SignUpForm}>
+                </Route>
+                <Route path="/login" component={LogInForm}>
+                </Route>
+                <Route path="/createticket" component={CreateTicketForm}>
+                </Route>
+                <Route path="/tickethistory" component={TicketHistoryForm}>
+                </Route>
+                <Route path="/myaccount" component={MyAccountForm}>
+                </Route>
+                <Route path="/" component={e => generateAlert()}>
+                </Route>
             </div>
           </div>
-          <Route path="/" component={e => generateAlert()}>
-          </Route>
         </div>
       </Router>
     );
