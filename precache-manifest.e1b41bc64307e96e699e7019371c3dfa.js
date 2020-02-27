8dec51d82eb62c659f21336c9e2b452e72add272
Andrew Canafe <andrew.g.canafe@gmail.com>
2020-02-27 15:24:09 -0800

    Updates

---
+++ precache-manifest.e1b41bc64307e96e699e7019371c3dfa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e849fc8fac116b7d70d9292a68a28871",
+    "url": "/react-auth-ui/index.html"
+  },
+  {
+    "revision": "9d78fa8ec034266915b4",
+    "url": "/react-auth-ui/static/css/main.e4e58d71.chunk.css"
+  },
+  {
+    "revision": "46b404554755f6ecd7a7",
+    "url": "/react-auth-ui/static/js/2.7deb357b.chunk.js"
+  },
+  {
+    "revision": "e666d2d60c0f5d6398f074a3b13a79a0",
+    "url": "/react-auth-ui/static/js/2.7deb357b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d78fa8ec034266915b4",
+    "url": "/react-auth-ui/static/js/main.4d92973a.chunk.js"
+  },
+  {
+    "revision": "f9abc40808eea8f6536d",
+    "url": "/react-auth-ui/static/js/runtime-main.eb1d1412.js"
+  },
+  {
+    "revision": "018f64ecee681585113c2aae685ce7c6",
+    "url": "/react-auth-ui/static/media/inprogress.018f64ec.png"
+  }
+]);
